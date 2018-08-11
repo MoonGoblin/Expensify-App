9a1a018de4e3109d71260b14f05cd24267e0b7c1
David Rytell <daverytell@gmail.com>
2018-08-11 10:35:55 -0700

    Lecture 79 - setting up regular links

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
@@ -30,20 +30,40 @@ const HelpPage = () => (
 
 const NotFoundPage = () => (
     <div>
-        404!
+        404 - <Link to="/">Go Home</Link>
     </div>
 );
 
+// Challenge
+// add a link that goes to home page in header.
+// add a link to the create expense page
+// add a link to the edit page
+// add a link to the help page
+
+const Header = () => (
+    <header>
+        <h1>Expensify</h1>
+        <Link to="/">Dashboard</Link>
+        <Link to="/create">Create Expense</Link>
+        <Link to="/edit">Edit Expense</Link>
+        <Link to="/help">Help</Link>
+    </header>
+);
+
+// Need div because header can't be out there on its own with BrowserRouter
 const routes = (
     <BrowserRouter>
-        <Switch> //stops when it finds a match
-            <Route path="/" component={ExpenseDashboardPage} exact={true}/>
-            <Route path="/create" component={AddExpensePage}/>
-            <Route path="/edit" component={EditExpensePage}/>
-            <Route path="/help" component={HelpPage}/>
-            <Route component={NotFoundPage}/>
-        </Switch>
+        <div> 
+            <Header />
+            <Switch> //stops when it finds a match
+                <Route path="/" component={ExpenseDashboardPage} exact={true}/>
+                <Route path="/create" component={AddExpensePage}/>
+                <Route path="/edit" component={EditExpensePage}/>
+                <Route path="/help" component={HelpPage}/>
+                <Route component={NotFoundPage}/>
+            </Switch>
 
+        </div>
     </BrowserRouter>
 );
 
