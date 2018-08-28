03eab12c065dd82610d5e903303bd1ac83056eec
David Rytell <daverytell@gmail.com>
2018-08-27 20:49:01 -0700

    Halfway through lecture 86 - starting redux actions

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,7 +1,30 @@
 import { createStore } from 'redux';
 
-const store = createStore((state = { count: 0 }) => {
+const store = createStore((state = { count: 0 }, action) => {
+    if(action.type === 'INCREMENT') {
+        return {
+            count: state.count + 1 
+        }
+    } else {
     return state;
+    }
 });
+
+console.log(store.getState());
+
+// Actions
+
+// increment, decrement, reset
+store.dispatch({
+    //type: 'INCREMENT_OTHER' // if you have a second word, you use underscore
+    type: 'INCREMENT'
+});
+
+store.dispatch({
+    type: 'INCREMENT'
+});
+
+// I'd like to reset the count
+// I'd like to reset the count to zero
 
 console.log(store.getState());
