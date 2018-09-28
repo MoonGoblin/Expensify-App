bf18303b82be1fc34e08fd4b9c2d7d591337b6f7
David Rytell <daverytell@gmail.com>
2018-09-27 21:13:57 -0700

    Lecture 110 - Filtering by Dates added

--- src/reducers/filters.js
+++ src/reducers/filters.js
@@ -1,10 +1,13 @@
+import moment from 'moment';
+
+
 // Filters Reducer
 // text => empty string, sortBy => date, startDate => undefined, endDate => undefined
 const filtersReducerDefaultState = {
     text: '',
     sortBy: 'date',
-    startDate: undefined,
-    endDate: undefined   
+    startDate: moment().startOf('month'),
+    endDate: moment().endOf('month')   
 };
 
 export default (state = filtersReducerDefaultState, action) => {
