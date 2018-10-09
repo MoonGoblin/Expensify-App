6fc325d4688bb8fef00bc1605b472cae77367975
David Rytell <daverytell@gmail.com>
2018-10-08 20:09:55 -0700

    Lecture 118 - Snapshot testing

---
+++ src/tests/components/Header.test.js
@@ -0,0 +1,14 @@
+// react-test-renderer
+import React from 'react';
+import ReactShallowRenderer from 'react-test-renderer/shallow';
+import Header from '../../components/Header';
+
+// shallow rendering only renders the given component
+
+
+test('should render Header correctly', () => {
+    const renderer = new ReactShallowRenderer();
+    renderer.render(<Header />);
+    expect(renderer.getRenderOutput()).toMatchSnapshot();
+    //console.log(renderer.getRenderOutput());
+});
